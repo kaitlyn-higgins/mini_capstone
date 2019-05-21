7675b926a094ec624c67d2795f56d7c0d8504da4
kaitlyn_higgins <kaitlyn.brooke.higgins@gmail.com>
2019-05-20 19:25:53 -0500

    Adds orders, users and suppliers to mini capstone, establishes connections

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,17 @@
 class Product < ApplicationRecord
 
+  has_many :orders
+
   belongs_to :supplier
 
   # def supplier
   #   Supplier.find_by(id: supplier_id)
   # end
+  has_many :images
 
 
-  # validates :name, :price, presence: true, uniqueness: true
-  # validates :price, numericality: {greater_than: 0}
+  validates :name, presence: true, uniqueness: true
+  validates :price, numericality: {greater_than: 0}
   # # validates :image_url, presence: true
   # validates :description, length: {in: 10..500}
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_165429) do
+ActiveRecord::Schema.define(version: 2019_05_20_204508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "images", force: :cascade do |t|
     t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "product_id"
+    t.decimal "subtotal", precision: 9, scale: 2
+    t.decimal "tax", precision: 9, scale: 2
+    t.decimal "total", precision: 9, scale: 2
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
