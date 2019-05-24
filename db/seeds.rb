ed9e4ce031425ee853035fc86b014d2f4ed37531
kaitlyn_higgins <kaitlyn.brooke.higgins@gmail.com>
2019-05-23 19:38:15 -0500

    Dumps images to seeds file, organizes logically

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,37 @@ Product.create!([
   {name: "Bean Bag Launcher", price: "30.0", description: "Use with caution", stock_status: true, supplier_id: 3},
   {name: "Party Streamer", price: "3.0", description: "Brighten up your event space", stock_status: true, supplier_id: 2}
 ])
+Image.create!([
+  {url: "https://sc02.alicdn.com/kf/HTB1zR3Ng3oQMeJjy0Foq6AShVXaK/200571711/HTB1zR3Ng3oQMeJjy0Foq6AShVXaK.jpg", product_id: 1},
+  {url: "https://media.giphy.com/media/fhA7UQ25NiFgs/giphy.gif", product_id: 1},
+  {url: "https://www.fullcompass.com/common/products/lgr/197352.jpg", product_id: 2},
+  {url: "https://media1.tenor.com/images/22ba3ec3ee6573e3607dfce231ac5f5a/tenor.gif?itemid=10142412", product_id: 2},
+  {url: "https://smhttp-ssl-43310.nexcesscdn.net/media/extendware/ewimageopt/media/inline/c0/5/chauvet-dj-nimbus-professional-dry-ice-smoke-fog-machine-store-display-model-846.jpg", product_id: 2},
+  {url: "https://images-na.ssl-images-amazon.com/images/I/61Z8ZMKeqHL._SX466_.jpg", product_id: 3},
+  {url: "https://www.stagelightingstore.com/23698-large_default/adj-f-scents-for-fog-juice-1-oz-apple.jpg", product_id: 3},
+  {url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuyt_Ocn5Nzv0hyHGQo9abPIBgsru8TroQ2FPtCHEdh3t5J7ozcQ", product_id: 4},
+  {url: "https://bmeeventgroup.com/wp-content/uploads/2019/02/lighting-1.jpg", product_id: 4},
+  {url: "http://globalentertainmentsource.com/wp-content/uploads/2017/10/uplights-outdoor-2.jpg", product_id: 4},
+  {url: "https://www.triaddj.com/wp-content/uploads/2012/06/UplightAfter.jpg", product_id: 4},
+  {url: "http://adelaideprodjs.com.au/wp-content/gallery/Up%20Lights/1.jpg", product_id: 4},
+  {url: "https://images-na.ssl-images-amazon.com/images/I/81UO84WJSNL._SX425_.jpg", product_id: 5},
+  {url: "https://media0.giphy.com/media/26gsnoFzafTMW26xq/giphy.gif", product_id: 5},
+  {url: "https://media0.giphy.com/media/oyQ5Qf9Ihu3ctAe4hw/giphy.gif", product_id: 5},
+  {url: "http://voidbenelux.com/files/2013/04/void_airmotion.jpg", product_id: 6},
+  {url: "http://voidbenelux.com/files/2017/07/14725753_10154040150003157_6861468665291121936_n.jpg", product_id: 6},
+  {url: "http://voidbenelux.com/files/2017/07/https://www.prodj.lt/wp-content/uploads/2018/07/5.jpg.jpg", product_id: 6},
+  {url: "https://i.pinimg.com/originals/b3/3a/bd/b33abdb9fa127324b18fc96a27b9db29.jpg", product_id: 7},
+  {url: "https://thefancy-media-ec1.thefancy.com/original/20121207/247587797840436783_9170fb499fb2.jpg", product_id: 7},
+  {url: "http://i41.tinypic.com/dggi15.jpg", product_id: 7},
+  {url: "https://tshop.r10s.com/be3/ced/2f8b/9282/b0d2/15ca/393e/111ae88dc654ab3a295add.jpg", product_id: 8},
+  {url: "https://img-aws.ehowcdn.com/877x500p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/31c45edd8cb34720a6d69be2ddaee445", product_id: 9},
+  {url: "https://www.dhresource.com/0x0s/f2-albu-g5-M01-85-AF-rBVaJFhMu9aAK8ihAAIonh37dsw745.jpg/12-100-pcs-lots-assorted-balloons-latex-polka.jpg", product_id: 9},
+  {url: "http://cdn.lowgif.com/full/a96c106a43306023-balloons-gif-tumblr-www-pixshark-com-images-galleries.gif", product_id: 9},
+  {url: "https://starfisheducation.com.au/wp-content/uploads/2016/06/DSC_5828.jpg", product_id: 10},
+  {url: "https://images-na.ssl-images-amazon.com/images/I/81ip1cgkZNL._SL1500_.jpg", product_id: 11},
+  {url: "https://images-na.ssl-images-amazon.com/images/I/61lRrOX456L._SX425_.jpg", product_id: 11},
+  {url: "https://ae01.alicdn.com/kf/HTB120eFSpXXXXaSXVXXq6xXFXXX9/GiveU-10rolls-lot-Total-250m-Crepe-Paper-Streamer-Wedding-Birthday-Party-Babyshower-Background-Decoration-Party-Streamers.jpg_640x640.jpg", product_id: 11}
+])
 User.create!([
   {name: "Shane", email: "shane@gmail.com", password_digest: "$2a$10$kVUQ5Rcss9jb7HjNIXKDRe0FxtV5WvBo5MSzoxGGS1fvvWysTGJ2a", admin: false},
   {name: "Kaitlyn", email: "kaitlyn@gmail.com", password_digest: "$2a$10$PYuGLs/hNtMc8RKLltfAk.W7Ul/B8cWiaKFF5zJwecdhUfb49kigO", admin: true}
@@ -55,3 +86,4 @@ ProductCategory.create!([
   {product_id: 10, category_id: 4},
   {product_id: 5, category_id: 5}
 ])
+
